fcd280a4e3f08368c7d5fbb316d24627fc1371bb
Morgoth Bauglir <xxxserborxxx@gmail.com>
2023-03-15 11:39:27 +0300

    One batch file

--- Kovrigin/apply changes.bat
+++ Kovrigin/apply changes.bat
@@ -2,7 +2,7 @@ R:
 cd /Kovrigin/Git/ngmg/Kovrigin
 git add .
 git commit
-git tag -a v1.0 -m "my version 1.0"
+git tag -a tag_Kovrigin_v1.0 -m "Kovrigin version 1.0"
 git pull
 git push origin --tags
 pause
