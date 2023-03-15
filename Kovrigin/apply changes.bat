8723966da920a2f05a4eaf3ed872168951147a8a
Morgoth Bauglir <xxxserborxxx@gmail.com>
2023-03-15 11:50:36 +0300

    Changes

--- Kovrigin/apply changes.bat
+++ Kovrigin/apply changes.bat
@@ -2,8 +2,11 @@ R:
 cd /Kovrigin/Git/ngmg/Kovrigin
 git add .
 git commit
-git tag
 git pull
+git merge
+git add .
+git commit
+git tag -a -f tag_Kovrigin_v1.0 -m "Kovrigin version 1.0"
 git push origin --tags
 git push
 pause
