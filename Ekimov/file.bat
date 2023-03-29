14b7aad979df63f3d28031010fd9774fff1fa60a
Morgoth Bauglir <xxxserborxxx@gmail.com>
2023-03-29 10:32:04 +0300

    Commit with bat

--- Ekimov/file.bat
+++ Ekimov/file.bat
@@ -1,11 +1,13 @@
 R:
 cd \Ekimov\git\ngmg\Ekimov
 git add .
-git commit -m "New bat file"
+git commit -m "Commit with bat"
 git pull
+
 git merge
 git commit -m "New bat file"
 git tag -a Ekimov_tag -m "Ekimov bat"
+
 git push origin --tags
 git push
 pause
