15e77bab0eb5c21074f61bff460ab6b7d12aff07
Morgoth Bauglir <xxxserborxxx@gmail.com>
2023-03-29 11:31:24 +0300

    New_file

--- Ekimov/file.bat
+++ Ekimov/file.bat
@@ -1,13 +1,19 @@
 R:
 cd \Ekimov\git\ngmg\Ekimov
 git add .
-git commit -m "comment"
+git commit 
 git pull
 
+git add .
 git merge
-git commit -m "New bat file"
-git tag -a -f Ekimov_tag_v1.0 -m "Ekimov bat version 1.0"
 
+set /P My_Tag="My_Tag: "
+echo %My_Tag%
+set /P Komment="Komment_for_tag: "
+echo %Komment%
+git tag -a -f %My_Tag% -m "%Komment%"
+
+git commit
 git push origin --tags
 git push
 pause
