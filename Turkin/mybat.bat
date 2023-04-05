41df6269184649d5d4c35d6c1b36cdab2e8005f3
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-05 12:34:51 +0300

    fix

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -4,26 +4,29 @@ set /p commit_message="Enter message commit: "
 
 setlocal EnableDelayedExpansion
 set /a tag_number=0
-for /f "delims=" %%a in ('git tag ^| findstr /r /c:"turkin_tag_[0-9]*$" ^| sort') do (
+for /f "delims=" %%a in ('git tag ^| findstr /r /c:"turkin_tag_[0-9]*$" ^| sort /r') do (
     set "last_tag=%%a"
-	echo !last_tag!
-	echo "negr"
-    set /a tag_number=!last_tag:~-1!+1
-    echo !tag_number!
-	goto :break
+    @REM echo !last_tag!
+    set /a tag_number=!last_tag:~11!+1
+    @REM echo !tag_number!
+    goto :break
 )
 :break
-echo %tag_number%
-echo %last_tag%
+@REM echo %tag_number%
+@REM echo %last_tag%
 git add .
 git commit -m "%commit_message%"
 if !last_tag! == "" (
     git tag -a turkin_tag_0 -m "Description tag"
 ) else (
-    git tag -a turkin_tag_%tag_number% -m "Description tag"
+    git tag -a turkin_tag_!tag_number! -m "Description tag"
 )
 git pull origin main
 git push origin main --tags
 
 echo "Change success."
 pause
+
+
+
+
