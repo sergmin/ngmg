0c50bdb35c08cd0ab28c3f597248446e63d82f6d
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-05 11:33:39 +0300

    fixed bat

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -1,11 +1,17 @@
-@echo off
-
 set /p commit_message="Enter message commit: "
 
+setlocal EnableDelayedExpansion
+set /a tag_number=0
+for /f "delims=" %%a in ('git tag ^| sort /r') do (
+    set "last_tag=%%a"
+    set /a tag_number=!last_tag:~-1!+1
+    goto :break
+)
+:break
 git add .
 git commit -m "%commit_message%"
-git tag -a v1.0 -m "Description tag"
+git tag -a v1.%tag_number% -m "Description tag"
 git push origin main --tags
 
-echo "Change success"
+echo "Change success."
 pause
