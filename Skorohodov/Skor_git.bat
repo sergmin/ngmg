2b65cb94fcdb13895050067fe47fb09fccd9464d
SkorEgor <92719975+SkorEgor@users.noreply.github.com>
2023-04-19 11:28:44 +0300

    new_commit_Egor

--- Skorohodov/Skor_git.bat
+++ Skorohodov/Skor_git.bat
@@ -4,5 +4,7 @@ cd D:\5)Development\8_git\ngmg\Skorohodov
 git pull
 git add --all
 git commit -m "new_commit_Egor"
-git tag -a v1 -m "new_tag_Egor"
 git push
+git tag "new_tag_Egor"
+git push origin --tags
+pause >nul
