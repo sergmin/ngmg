b5d2e9574ff8f6386a294d8019094077783c30d1
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-27 18:17:29 +0300

    fix

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -6,12 +6,12 @@ setlocal EnableDelayedExpansion
 set /a tag_number=0
 for /f "tokens=2 delims=" %%a in ('git tag ^| findstr /r /c:"turkin_tag[0-9]*$" ^| sort /r') do (
 set "last_tag=%%a"
-set /a tag_number=!last_tag:~-1!+1
+set /a tag_number=%last_tag:~-1%
 goto :break
 )
 
 @REM echo %tag_number%
-set /a tag_number2=%tag_number%+1
+set /a tag_number2=tag_number+1
 echo %tag_number2%
 git add .
 git commit -m "%commit_message%"
