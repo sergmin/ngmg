4cd15444466f6f778394b8856427276ae154d373
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-27 19:24:21 +0300

    fix

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -9,11 +9,12 @@ set "last_tag_number=0"
 
 for /f "tokens=2 delims=_" %%a in ('git tag ^| findstr /r /c:"turkin_tag_[0-9]*$" ^| sort /r') do (
     set "last_tag=%%a"
-    set /a "last_tag_number=%%a+1"
+    set /a "last_tag_number=%%a"
     goto :break
 )
 
-set "new_tag=turkin_tag_%last_tag_number%"
+set /a "new_tag_number=last_tag_number+1"
+set "new_tag=turkin_tag_%new_tag_number%"
 
 echo New tag: %new_tag%
 
