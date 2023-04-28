0e70429ac01523809a5603f055af6d7e46a62d04
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 16:04:07 +0300

    fix

--- Turkin/add_tag.py
+++ Turkin/add_tag.py
@@ -1,25 +1,12 @@
 import subprocess
 import re
 
-# Обновляем информацию о тегах в локальном репозитории
+
 subprocess.check_call(['git', 'fetch', '--tags'])
-
-# Получаем список существующих тегов
 tags = subprocess.check_output(['git', 'tag']).decode().split()
-
-# Ищем последний тег вида "turkin_tag*"
 last_tag = max((t for t in tags if t.startswith('turkin_tag')), default='turkin_tag_0')
-
-# Получаем номер последнего тега
-# last_number = int(last_tag.split('turkin_tag')[-1])
 last_number = int(re.findall(r'\d+', last_tag)[-1])
-
-# Проверяем есть ли изменения в удаленном репозитории и обновляем локальный репозиторий
 subprocess.check_call(['git', 'pull'])
-
-# Создаем новый тег с номером на 1 больше последнего
 new_tag = f"turkin_tag_{last_number+1}"
 subprocess.check_call(['git', 'tag', new_tag])
-
-# Отправляем тег в удаленный репозиторий
 subprocess.check_call(['git', 'push', '--tags'])
--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -1,8 +1,11 @@
 @echo off
 setlocal
+
 set /p commit_message="Enter commit message: "
 git add .
 git commit -m "%commit_message%"
 git push origin main
+
 python add_tag.py
+
 pause
