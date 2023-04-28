4604269232b145de36bbfde1a0d7e24cd273ae80
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 13:19:02 +0300

    Auto-commit before creating new tag

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -4,6 +4,8 @@ setlocal enabledelayedexpansion
 set prefix=turkin_tag_
 set /a max_tag_number=-1
 
+echo Searching for existing tags...
+
 for /f "tokens=1,2 delims=_" %%i in ('git tag -l "turkin_tag_*"') do (
     if "%%j" neq "" (
         set "current_tag_number=%%j"
@@ -15,8 +17,13 @@ for /f "tokens=1,2 delims=_" %%i in ('git tag -l "turkin_tag_*"') do (
 set /a next_tag_number=max_tag_number+1
 set next_tag=%prefix%!next_tag_number!
 
+echo Creating new tag %next_tag%...
+
 git add .
 git commit -m "Auto-commit before creating new tag"
 git tag %next_tag%
 echo Created tag %next_tag%
 git push --tags
+
+echo Script execution complete. Press any key to exit.
+pause >nul
