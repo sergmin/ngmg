30c8d144f462927de8ddad80e470de9f31ddf3bd
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 12:49:43 +0300

    fix

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -4,20 +4,17 @@ set /p commit_message="Enter commit message: "
 
 setlocal EnableDelayedExpansion
 
-set "last_tag=turkin_tag_"
 set "last_tag_number=0"
 
 for /f "tokens=2 delims=_" %%a in ('git tag ^| findstr /r /c:"turkin_tag_[0-9]*$" ^| sort /r') do (
-    set "last_tag=%%a"
-    set "last_tag_number=%%a"
+    set /a "last_tag_number=%%a"
     goto :break
 )
 
-echo Last tag: %last_tag%
+echo Last tag number: %last_tag_number%
 
-set /a tag_number=last_tag_number+1
-if %tag_number%==1 set "new_tag=turkin_tag_!tag_number!"
-if %tag_number% gtr 1 set "new_tag=turkin_tag_%tag_number%"
+set /a "tag_number=last_tag_number+1"
+set "new_tag=turkin_tag_%tag_number%"
 
 echo New tag: %new_tag%
 
