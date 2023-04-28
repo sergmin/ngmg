c70c86843d36c72cd5f98a4d52a2d6f9a642c374
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 15:46:17 +0300

    fix

--- Turkin/add_tag.py
+++ Turkin/add_tag.py
@@ -1,9 +1,7 @@
 import subprocess
 
-# Обновляем информацию о тегах в локальном репозитории
-subprocess.check_call(['git', 'fetch', '--tags'])
-
 # Получаем список существующих тегов
+subprocess.check_call(['git', 'fetch', '--tags'])
 tags = subprocess.check_output(['git', 'tag']).decode().split()
 
 # Ищем последний тег вида "turkin_tag*"
@@ -13,12 +11,16 @@ last_tag = max((t for t in tags if t.startswith('turkin_tag')), default='turkin_
 # last_number = int(last_tag.split('turkin_tag')[-1])
 last_number = int(''.join(filter(str.isdigit, last_tag)))
 
-# Проверяем есть ли изменения в удаленном репозитории и обновляем локальный репозиторий
-subprocess.check_call(['git', 'pull'])
-
-# Создаем новый тег с номером на 1 больше последнего
+# Проверяем, есть ли уже тег с таким номером
 new_tag = f"turkin_tag_{last_number+1}"
-subprocess.check_call(['git', 'tag', new_tag])
+if new_tag in tags:
+    print(f"Тег {new_tag} уже существует.")
+else:
+    # Проверяем есть ли изменения в удаленном репозитории и обновляем локальный репозиторий
+    subprocess.check_call(['git', 'pull'])
 
-# Отправляем тег в удаленный репозиторий
-subprocess.check_call(['git', 'push', '--tags'])
+    # Создаем новый тег с номером на 1 больше последнего
+    subprocess.check_call(['git', 'tag', new_tag])
+
+    # Отправляем тег в удаленный репозиторий
+    subprocess.check_call(['git', 'push', '--tags'])
