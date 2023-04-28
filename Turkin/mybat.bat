bebc86f4a30a9263697d5dc69506376dcdb9fe53
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 15:12:46 +0300

    Added new tag

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -11,23 +11,29 @@ for /f "delims=" %%f in ('dir /b turkin_tag*') do (
   if !tag! neq !tag:nonumber=!
   (
     :: находим максимальный номер тега
-    if !tag! gtr !max_tag! set max_tag=!tag!
+    if not defined max_tag set max_tag=!tag!
+    if !tag! gtr !max_tag set max_tag=!tag!
   )
   endlocal
 )
 
-:: создаём новый тег с номером max_tag+1
-set /a new_tag=max_tag+1
-git tag turkin_tag%new_tag%
+:: создаем новый тег с номером max_tag+1
+if %max_tag% equ 0 (
+  git tag turkin_tag1
+) else (
+  set /a new_tag=max_tag+1
+  git tag turkin_tag%new_tag%
+)
 
 :: добавляем все изменения в индекс
 git add -A
 
 :: создаём коммит
-git commit -m "Added tag turkin_tag%new_tag%"
+git commit -m "Added new tag"
 
 :: отправляем изменения на удалённый сервер в ветку main
 git push origin main
 
 :: выводим сообщение
+echo "New tag added"
 pause
