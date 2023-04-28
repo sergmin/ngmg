bfe64bb48c8af9750346416479352625e5272dc7
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 12:49:55 +0300

    fix

--- Turkin/mybat.bat
+++ Turkin/mybat.bat
@@ -7,14 +7,16 @@ setlocal EnableDelayedExpansion
 set "last_tag_number=0"
 
 for /f "tokens=2 delims=_" %%a in ('git tag ^| findstr /r /c:"turkin_tag_[0-9]*$" ^| sort /r') do (
-    set /a "last_tag_number=%%a"
-    goto :break
+    set "last_tag=%%a"
+    set "last_tag_number=%%a"
+    exit /b
 )
 
-echo Last tag number: %last_tag_number%
+echo Last tag: %last_tag%
 
-set /a "tag_number=last_tag_number+1"
-set "new_tag=turkin_tag_%tag_number%"
+set /a tag_number=last_tag_number+1
+if %tag_number%==1 set "new_tag=turkin_tag_!tag_number!"
+if %tag_number% gtr 1 set "new_tag=turkin_tag_%tag_number%"
 
 echo New tag: %new_tag%
 
@@ -26,5 +28,3 @@ git push origin main --tags
 
 echo "Change success."
 pause
-
-:break
