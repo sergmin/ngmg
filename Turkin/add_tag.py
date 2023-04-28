75754feb8ae6025f5b21aa735b8afb2eabd672b2
Igor Turkin <igor.turkin@alarislabs.com>
2023-04-28 15:55:39 +0300

    fix

--- Turkin/add_tag.py
+++ Turkin/add_tag.py
@@ -7,9 +7,7 @@ subprocess.check_call(['git', 'fetch', '--tags'])
 tags = subprocess.check_output(['git', 'tag']).decode().split()
 
 # Ищем последний тег вида "turkin_tag*"
-last_tag = max((t for t in tags if t.startswith('turkin_tag')), default='turkin_tag_0')
-# turkin_tags = [t for t in tags if t.startswith('turkin_tag')]
-# last_tag = sorted(turkin_tags)[-1] if turkin_tags else 'turkin_tag_0'
+last_tag = max((t for t in tags if t.startswith('turkin_tag')), default='turkin_tag0')
 
 # Получаем номер последнего тега
 # last_number = int(last_tag.split('turkin_tag')[-1])
