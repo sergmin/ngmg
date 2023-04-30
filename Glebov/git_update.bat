90a8def875e844bf1a817b5337a5969cf10d61cb
folga <dcedscegvr>
2023-04-30 15:44:52 +0300

    update

---
+++ Glebov/git_update.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set /p commit_message="Enter commit message: "
+
+setlocal EnableDelayedExpansion
+set /a tag_number=0
+for /f "delims=" %%a in ('git tag ^| findstr /r /c:"Glebov_tag_[0-9]*$" ^| sort') do (
+    set "last_tag=%%a"
+    set /a tag_number=!last_tag:~-1!
+)
+set /a tag_number+=1
+git add .
+git commit -m "%commit_message%"
+git tag -a Glebov_tag_%tag_number% -m "Description tag"
+git pull origin main
+git push origin main --tags
+
+echo "Change success."
+pause
