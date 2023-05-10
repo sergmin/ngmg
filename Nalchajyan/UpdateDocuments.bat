95d3e8d964374e38cb777cbe1430b4ed5489c188
MariamNalchajyan <66250509+MariamNalchajyan@users.noreply.github.com>
2023-05-10 11:00:46 +0300

    Update UpdateDocuments.bat

--- Nalchajyan/UpdateDocuments.bat
+++ Nalchajyan/UpdateDocuments.bat
@@ -4,7 +4,6 @@ cd C:\\Mariam\\MyGit\\ngmg
 git pull
 
 pandoc --pdf-engine=xelatex -V mainfont="Times New Roman" C:\\Mariam\\MyGit\\ngmg.wiki\\Спецификация.md -o C:\\Mariam\\MyGit\\ngmg\\Documents\\Спецификация.pdf
-pandoc --pdf-engine=xelatex -V mainfont="Times New Roman" C:\\Mariam\\MyGit\\ngmg.wiki\\Текст-программы.md -o C:\\Mariam\\MyGit\\ngmg\\Documents\\Текст-программы.pdf
 pandoc --pdf-engine=xelatex -V mainfont="Times New Roman" C:\\Mariam\\MyGit\\ngmg.wiki\\Описание-программы.md -o C:\\Mariam\\MyGit\\ngmg\\Documents\\Описание-программы.pdf
 pandoc --pdf-engine=xelatex -V mainfont="Times New Roman" C:\\Mariam\\MyGit\\ngmg.wiki\\Описание-применения.md -o C:\\Mariam\\MyGit\\ngmg\\Documents\\Описание-применения.pdf
 pandoc --pdf-engine=xelatex -V mainfont="Times New Roman" C:\\Mariam\\MyGit\\ngmg.wiki\\Руководство-программиста.md -o C:\\Mariam\\MyGit\\ngmg\\Documents\\Руководство-программиста.pdf
